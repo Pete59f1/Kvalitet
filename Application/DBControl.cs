a88afabcfd06160700fea698dac6afe85fac6160
Michal Tomasz Odzienkowski <mich040m@edu.eal.dk>
2019-02-21 08:19:14 +0100

    DBController CreateCustomer method

--- Application/DBControl.cs
+++ Application/DBControl.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace MyApplication
 {
-    class DBControl
+    internal class DBControl
     {
+        private static string connectionString = 
+            "Server=michaldatabase.database.windows.net; Database= KvalitetProject; User Id=sasjumb; Password=SuperPassword123;";
+        public void CreateCustomer(string name, string address, int zip, string town, string tlf)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("spCreateCustomer", con);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+
     }
 }
