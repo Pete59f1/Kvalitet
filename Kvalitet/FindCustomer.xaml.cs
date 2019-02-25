0402a8ceb82fda7f9e7c01c819b90c09ef34bf44
Peter Starup <pete59f1@edu.eal.dk>
2019-02-25 11:35:53 +0100

    FindCustomer

--- Kvalitet/FindCustomer.xaml.cs
+++ Kvalitet/FindCustomer.xaml.cs
@@ -19,9 +19,39 @@ namespace Kvalitet
     /// </summary>
     public partial class FindCustomer : Window
     {
+        MyApplication.Control con;
         public FindCustomer()
         {
+            con = new MyApplication.Control();
             InitializeComponent();
+        }
+
+        private void BtnFindCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            if (con.FindCustomer(int.Parse(txtCustomerId.Text)) == "fejl")
+            {
+                btnCreateCustomer.Visibility = Visibility;
+                lblFoundCustomer.Visibility = Visibility;
+                lblFoundCustomer.Content = "Ingen kunde fundet. Opret ny kunde";
+            }
+            else
+            {
+                btnCreateOrder.Visibility = Visibility;
+                lblFoundCustomer.Visibility = Visibility;
+                lblFoundCustomer.Content = "Kunde fundet. Opret ny ordre";
+            }
+        }
+
+        private void BtnCreateCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            CreateCustomer cc = new CreateCustomer();
+            cc.Show();
+        }
+
+        private void BtnCreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOrder co = new CreateOrder();
+            co.Show();
         }
     }
 }
