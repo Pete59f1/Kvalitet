f5edefbbdf40a270bc500a349b4f65f3cb41d5ce
Michal Tomasz Odzienkowski <mich040m@edu.eal.dk>
2019-02-25 12:35:22 +0100

    BLa

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -13,13 +13,13 @@ namespace Domain
         private int orderId;
         private DateTime orderDate;
         private DateTime deliveryDate;
-        private int picked;
+        private bool picked;
 
         public Customer Customer { get { return customer; } set { customer = value; } }
         public int OrderId { get { return orderId; } set { orderId = value; } }
         public DateTime OrderDate { get { return orderDate; } set { orderDate = value; } }
         public DateTime DeliveryDate { get { return deliveryDate; } set { deliveryDate = value; } }
-        public int Picked { get { return picked; } set { picked = value; } }
+        public bool Picked { get { return picked; } set { picked = value; } }
         private List<SaleOrderLine> OrderLines { get { return orderLines; } set { orderLines = value; } }
 
         public void AddOrderLine(Product product, int quantity, double price)
